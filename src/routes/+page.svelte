3a65bd981ef1c8582e9da286643a9de2967b69d1
Yoe <46240707+marylkayoe@users.noreply.github.com>
2025-03-16 18:35:35 +0900

    set markdown as homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Post from './posts/2025-03-16-welcome.md';
+  </script>
+  
+  <Post />
