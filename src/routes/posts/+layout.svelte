25c3930ee5b30eb58b270a72b8cc45daa0535adf
Yoe <46240707+marylkayoe@users.noreply.github.com>
2025-03-17 19:58:17 +0900

    layout update

--- src/routes/posts/+layout.svelte
+++ src/routes/posts/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
-    export let data;
-  </script>
-  
-  <main>
-    <article>
-      <slot />
-    </article>
-  </main>
+  export let data;
+</script>
+
+<main style="max-width:700px; margin:auto; font-family:sans-serif; line-height:1.6;">
+  <h1>{data.frontmatter.title}</h1>
+  <p style="color:#777; font-size:0.9rem;">{new Date(data.frontmatter.date).toDateString()}</p>
+  <article>
+    <slot />
+  </article>
+</main>
