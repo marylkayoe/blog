a662fc15f5face8bb6a9bf728ceae9ef3e401d39
marylkayoe <marylkayoe@users.noreply.github.com>
2025-04-12 10:10:36 +0000

    deploy: 0075eed12fa25259b84c52bd80b4e1d506f527ed

---
+++ _app/immutable/chunks/CvdKMBSN.js
@@ -0,0 +1,1 @@
+import{t as e,a as i}from"./DNVPeWcQ.js";import"./B5GBQSCi.js";const n={title:"Welcome Back!",date:"2025-03-16T00:00:00.000Z"},{title:p,date:l}=n;var o=e("<p>I’m blogging again, and this time with SvelteKit and markdown. It feels nice.</p>");function r(t){var a=o();i(t,a)}export{r as default,n as metadata};
---
+++ _app/immutable/chunks/Is_KoLbC.js
@@ -0,0 +1,2 @@
+import{t as o,a}from"./DNVPeWcQ.js";import"./B5GBQSCi.js";const e={title:"Mobile Thoughts",date:"2025-04-12",tags:["nerding out","testing"]},{title:l,date:r,tags:u}=e;var i=o(`<p>Just posting a thought quickly from my phone, to see if it works!
+And it does, poor github actions has to do all the heavy lifting… insane, to install a linux distro just to run a script that posts a file to a website. But hey, to get cat posts as fast as possible, it’s worth it, i guess</p>`);function h(t){var s=i();a(t,s)}export{h as default,e as metadata};
