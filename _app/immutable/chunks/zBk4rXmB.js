3156220fb6def551ab9cacc0380dae91419e5bf4
marylkayoe <marylkayoe@users.noreply.github.com>
2026-02-11 01:37:24 +0000

    deploy: 44f4f976edaf7497ca81181d9557d6b76a4d2ebc

---
+++ _app/immutable/chunks/zBk4rXmB.js
@@ -0,0 +1,4 @@
+import{t as e,a as r}from"./DNVPeWcQ.js";import"./B5GBQSCi.js";const s={title:"why ai doesn't art",date:"2026-02-11",tags:["thought","ai","art"]},{title:n,date:h,tags:p}=s;var o=e(`<p>Write your few lines here.
+art tells us that
+somewhere a human existed
+and was changed</p>`);function m(t){var a=o();r(t,a)}export{m as default,s as metadata};
